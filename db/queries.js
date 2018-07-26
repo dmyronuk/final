080ae3f72e5ce83164d61c4ec2ebf62f496c03e9
Darcy Myronuk <darcy.myronuk@gmail.com>
2018-07-26 22:35:50 +0000

    Add comma to prevent server crash and delete commented out vestigial queries

--- db/queries.js
+++ db/queries.js
@@ -24,34 +24,10 @@ module.exports = {
   getAllLandlords: () => {
     return knex('landlords')
       .select()
-  }
+  },
 
   getAllTenants: () => {
     return knex('tenants')
       .select()
-  }
-
-
-
-  // getAllStudents: () => {
-  //   return knex("students").select()
-  // },
-
-  // getStudentInfo: (student_id) => {
-  //   return knex("students")
-  //     .where("id", student_id)
-  //     .select()
-  // },
-
-  // getAllLessonsByStudent: (student_id) => {
-  //   return knex("lessons")
-  //     .where("student_id", student_id)
-  //     .select();
-  // },
-
-  // getLessonsInMonthByStudent: (student_id, startDate, endDate) => {
-  //   return knex("lessons")
-  //     .where("student_id", student_id)
-  //     .whereBetween("date", [startDate, "2020-01-01"])
-  // }
+  },
 }
