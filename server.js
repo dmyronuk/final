2f5a96b83927181d14b064ff7b166ead6883db4a
Frank Zhou <frank__zhou@hotmail.com>
2018-07-27 16:09:09 -0400

    added packges required for auth

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const app = express();
+const jwt = require('express-jwt');
+const jwks = require('jwks-rsa');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
 const PORT = 3001;
 
