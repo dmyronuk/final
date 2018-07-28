370194e1be566b5179ea9f53d37157f65c6e8ecd
Darcy Myronuk <darcy.myronuk@gmail.com>
2018-07-28 17:05:34 +0000

    On single listing component, add 3 buttons for yelp search of restaurants, schools and entertainment

--- client/src/rentals/RentalGridCard.js
+++ client/src/rentals/RentalGridCard.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const RentalGridCard = (props) => {
-  console.log("HEYYYY", props.data.city)
 return(
     <Link to={"/rentals/" + props.id}>
       <div className="grid-listing-card">
