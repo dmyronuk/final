7beefa102f39d6b479c4bb7e13db7aea38fbb3d2
charleswang234 <wang.charles234@gmail.com>
2018-07-30 23:59:58 +0000

    modified PACKAGE.JSON, and hashed password

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,14 +1,19 @@
 const queries = require("../db/queries");
 const helpers = require("../helpers/helpers.js");
+const bcrypt = require('bcrypt');
 
 let controller = {
 
   signup: function(req, res) {
     const data = req.body;
+    const hashed_password = bcrypt.hashSync(data.password, 10);
+    data.password = null;
+    data.password_confirmation = null;
+    data.hashed_password = hashed_password;
     console.log(data);
     queries.signup(data)
     .then(() => {
-      res.send("success");
+      res.json({status: "success"});
     })
   }
 
