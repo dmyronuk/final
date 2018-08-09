3e5fa2489f6baf3f9e1b85dc5e9c24c2eaa2c698
Frank Zhou <frank__zhou@hotmail.com>
2018-08-09 10:49:07 -0400

    changed get started to grid view

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -21,7 +21,7 @@ class Home extends Component {
           <h1> Welcome to Kiro </h1>
           <h2>It's better than Craigslist.</h2>
           <div id="start-button">
-            <Link to="/rentals/map">
+            <Link to="/rentals/grid">
               <Button
                 variant="raised"
                 size="large"
